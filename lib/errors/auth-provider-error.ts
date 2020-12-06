b2eb7ba33e7b08823f2037a70ef359963708d891
Max Holman <max@holmn.com>
2020-12-06 20:06:51 +0800

    fix: incorrect provider error code

--- lib/errors/auth-provider-error.ts
+++ lib/errors/auth-provider-error.ts
@@ -1,10 +1,10 @@
-import { CustomError } from '@block65/custom-error';
+import { CustomError, Status } from '@block65/custom-error';
 
 export class AuthProviderError extends CustomError {
   public constructor(message: string, err?: Error) {
     super(message, err);
     this.setName('AuthProviderError');
-    this.statusCode = 500;
+    this.statusCode = Status.UNAVAILABLE;
     this.sensitive = true;
   }
 }
