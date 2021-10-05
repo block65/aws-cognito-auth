587a775e387d365aafe634029de100a1a9389117
Max Holman <max@holmn.com>
2021-10-05 15:31:54 +0800

    chore: compat for latest @block65/custom-error

--- lib/errors/token-invalid-error.ts
+++ lib/errors/token-invalid-error.ts
@@ -1,10 +1,5 @@
 import { CustomError, Status } from '@block65/custom-error';
 
 export class TokenInvalidError extends CustomError {
-  public constructor(message: string, err?: Error) {
-    super(message, err);
-    this.setName('TokenError');
-    this.statusCode = Status.INVALID_ARGUMENT;
-    this.sensitive = true;
-  }
+  code = Status.INVALID_ARGUMENT;
 }
--- lib/errors/token-unsuitable-error.ts
+++ lib/errors/token-unsuitable-error.ts
@@ -1,9 +1,5 @@
 import { CustomError, Status } from '@block65/custom-error';
 
 export class TokenUnsuitableError extends CustomError {
-  public constructor(message: string, err?: Error) {
-    super(message, err);
-    this.setName('UnsuitableTokenError');
-    this.statusCode = Status.INVALID_ARGUMENT;
-  }
+  code = Status.INVALID_ARGUMENT;
 }
