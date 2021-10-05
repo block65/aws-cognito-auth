c2fc315651e1f962daa9fd1e99975262991f163c
Max Holman <max@holmn.com>
2021-10-05 15:31:54 +0800

    chore: compat for latest @block65/custom-error

--- lib/errors/token-expired-error.ts
+++ lib/errors/token-expired-error.ts
@@ -1,8 +1,6 @@
+import { Status } from '@block65/custom-error';
 import { TokenUnsuitableError } from './token-unsuitable-error.js';
 
 export class TokenExpiredError extends TokenUnsuitableError {
-  public constructor(message: string, err?: Error) {
-    super(message, err);
-    this.setName('ExpiredTokenError');
-  }
+  code = Status.UNAUTHENTICATED;
 }
